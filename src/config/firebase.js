a820effa5d533e87145dda6c27b8f61287756674
Gaurav Singh <139119394+GauRav-sINGh123@users.noreply.github.com>
2024-08-18 19:19:10 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,8 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
  
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "blog-85ce6.firebaseapp.com",
-  projectId: "blog-85ce6",
-  storageBucket: "blog-85ce6.appspot.com",
-  messagingSenderId: "214503324857",
-  appId: "1:214503324857:web:3a4d49c23c29d31fa2af68"
+ 
+ 
 };
 
 // Initialize Firebase
